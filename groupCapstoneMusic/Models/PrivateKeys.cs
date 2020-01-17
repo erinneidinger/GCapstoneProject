33944fbcce8f2a5a59e88a8eb84e0d4c8af54c02
Erinn Neidinger <erinndneidinger@gmail.com>
2020-01-17 11:25:13 -0600

    new gitignore and changes to PrivateKey class

---
+++ groupCapstoneMusic/Models/PrivateKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace groupCapstoneMusic.Models
+{
+    public static class PrivateKeys
+    {
+        //This is adam's google api code used to call our needed google api's i wrote a get ignore in txt file so it isn't pushed to github
+        //if you need to use it, it is a static class, so just call googleKey in replace of the actual code
+        //this way it will be hidden in the back end and wont be stolen.
+        //public static string googleKey = 
+    }
+}
