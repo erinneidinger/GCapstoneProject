c85af474920238aee3654da5e7e4499acdf58394
Michael Gonzalez Jr <mikegjr95@gmail.com>
2020-01-17 13:28:10 -0600

    helping fix an issue in event controller and added views for musician

--- groupCapstoneMusic/Controllers/EventController.cs
+++ groupCapstoneMusic/Controllers/EventController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using groupCapstoneMusic.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,22 +8,22 @@ using System.Web.Mvc;
 
 namespace groupCapstoneMusic.Controllers
 {
-    public class EventController : Controller
-    {
+//    public class EventController : Controller
+//    {
 //        private ApplicationDbContext db = new ApplicationDbContext();
 //        // GET: Event
 //        public ActionResult Index()
 //        {
 //            var Id = User.Identity.GetUserId();
 //            var foundEvent = db.Events.Where(a => a.ApplicationId == Id).FirstOrDefault();
-//            var oneEvent = db.Events.Where(a => a.EventId == foundEvent.EventId).ToList();
+//            var oneEvent = db.Events.Where(a => a.Id == foundEvent.Id).ToList();
 //            return View();
 //        }
 
 //        // GET: Event/Details/5
 //        public ActionResult Details(int id)
 //        {
-//            var eventDetails = db.Events.Where(a => a.EventId == id).FirstOrDefault();
+//            var eventDetails = db.Events.Where(a => a.Id == id).FirstOrDefault();
 //            return View(eventDetails);
 //        }
 
@@ -41,13 +43,13 @@ namespace groupCapstoneMusic.Controllers
 //                // TODO: Add insert logic here
 //                string userId = User.Identity.GetUserId();
 //                event.ApplicationId = userId;
-//        db.Events.Add(event);
-//        db.SaveChanges();
+//                db.Events.Add(event);
+//                db.SaveChanges();
 //                return RedirectToAction("Index");
 //    }
 //            catch
 //            {
-//                return View();
+//                return View(); 
 //}
 //        }
 
@@ -95,5 +97,5 @@ namespace groupCapstoneMusic.Controllers
 //        return View();
 //    }
 //}
-    }
+//    }
 }
