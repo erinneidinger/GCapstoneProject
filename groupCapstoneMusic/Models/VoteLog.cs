56dc9065c2a4ceddcac3158d8f67fd090f831ddd
Adam <dev.adammilbrath@gmail.com>
2020-01-17 13:16:45 -0600

    created votelog.cs and need Adam to migrate to avoid prior issues. created shared view VoteShow.

---
+++ groupCapstoneMusic/Models/VoteLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace groupCapstoneMusic.Models
+{
+    public class VoteLog
+    {
+        [Key]
+        public int AutoId { get; set; }
+        public int SectionId { get; set; }
+        public int VoteForId { get; set; }
+        public string UserName { get; set; }
+        public int Rating { get; set; }
+        public bool Active { get; set; }
+    }
+}
