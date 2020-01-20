a406e7a97eb4f4743f745ae6df67abeb09f7aad9
Erinn Neidinger <erinndneidinger@gmail.com>
2020-01-20 16:48:57 -0600

    working on dropdown filter search

--- groupCapstoneMusic/Models/Customer.cs
+++ groupCapstoneMusic/Models/Customer.cs
@@ -45,8 +45,8 @@ namespace groupCapstoneMusic.Models
         [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
         [StringLength(5)]
         public string Rating { get; set; }
-
-        public List<Concert> events { get; set; }
+       
+        public List<Concert> gigs { get; set; }
 
 
         //public int RateCount //KEEP COMMMED OUT TILL IT GETS WORKING FOR TESTING PURP
