f0a8b04278736b26c945ab05899187578e18daf9
Erinn Neidinger <erinndneidinger@gmail.com>
2020-01-20 09:30:53 -0600

    Working on google maps API in Concert Details view

--- groupCapstoneMusic/Controllers/ConcertController.cs
+++ groupCapstoneMusic/Controllers/ConcertController.cs
@@ -30,6 +30,7 @@ namespace groupCapstoneMusic.Controllers
         public ActionResult Details(int id)
         {
             var concertDetails = db.Concerts.Where(a => a.Id == id).FirstOrDefault();
+            GetLatNLngAsync(concertDetails);
             return View();
         }
 
