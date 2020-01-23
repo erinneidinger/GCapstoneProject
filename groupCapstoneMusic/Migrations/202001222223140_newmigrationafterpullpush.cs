c2d3ba6197dfd74bb6ded1b673e4667a7e35d0ae
Adam <dev.adammilbrath@gmail.com>
2020-01-23 08:33:30 -0600

    working on styling and creating the WhoWeAre view page for us

---
+++ groupCapstoneMusic/Migrations/202001222223140_newmigrationafterpullpush.cs
@@ -0,0 +1,22 @@
+namespace groupCapstoneMusic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class newmigrationafterpullpush : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Concerts", "Concert_Id", c => c.Int());
+            CreateIndex("dbo.Concerts", "Concert_Id");
+            AddForeignKey("dbo.Concerts", "Concert_Id", "dbo.Concerts", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Concerts", "Concert_Id", "dbo.Concerts");
+            DropIndex("dbo.Concerts", new[] { "Concert_Id" });
+            DropColumn("dbo.Concerts", "Concert_Id");
+        }
+    }
+}
