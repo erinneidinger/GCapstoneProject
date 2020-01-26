a1d67408a5c4ae2bdb1f7fd3e83bce10a4ba5d18
Adam <dev.adammilbrath@gmail.com>
2020-01-26 16:17:27 -0600

    fixed graphics for star rating and removed ability to rate as 0

---
+++ groupCapstoneMusic/Migrations/202001242204563_datnewwwwwwww.cs
@@ -0,0 +1,16 @@
+namespace groupCapstoneMusic.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class datnewwwwwwww : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
